71caf12b99e5a6dcd0962a857337311956556530
Yadav Dhakal <yadavdhkl@gmail.com>
2021-02-24 18:23:22 -0600

    Project initial setup, class and initial migration

---
+++ Park/Park.Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Park.Core.Models;
+
+namespace Park.Infra.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<NationalPark> NationalParks { get; set; }
+    }
+}
+
