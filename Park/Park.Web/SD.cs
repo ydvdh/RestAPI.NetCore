2e314a83f7a3cf3abbd99ef802dc73f0e0a742ac
Yadav Dhakal <yadavdhkl@gmail.com>
2021-02-28 21:18:51 -0600

    Repository Interface and implementation

---
+++ Park/Park.Web/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace Park.Web
+{
+    public class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44344/";
+        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalpark";
+        public static string TrailAPIPath = APIBaseUrl + "api/v1/trails";
+    }
+}
--- Park/Park.Web/Startup.cs
+++ Park/Park.Web/Startup.cs
@@ -24,6 +24,8 @@ namespace Park.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
